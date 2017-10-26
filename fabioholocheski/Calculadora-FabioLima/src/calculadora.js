d610c3e05bad20a50805fd28a1bed122dc72f437
Fabio Lima <fabioholocheski@gmail.com>
2017-10-26 21:43:28 -0200

    Adiciona função para os botoes de numeros

--- fabioholocheski/Calculadora-FabioLima/src/calculadora.js
+++ fabioholocheski/Calculadora-FabioLima/src/calculadora.js
@@ -1,8 +1,6 @@
 console.log(document.getElementById('calculadora-display'));
 
-function nomeDaFuncao() {
-  console.log("Estou dentro da função....");
-}
+
 
 var func = function(){
   console.log("função que esta na variavel");
@@ -11,11 +9,27 @@ var func = function(){
 function calcular() {
   alert("Deve mostrar o resultado da operação")
 }
-function valor(element, evento, val) {
-  console.log(element);
-  console.log(evento);
-  alert("voce clicou no botao   "  + val);
+function valor() {
+  console.log(this.value);
+}
+function limpar() {
+  console.log("Limpar...");
 }
 window.onload = function(){
   console.log("terminou de Carregar a Pagina");
+
+  //var calculadora = document.querySelectorAll(".btnCalc");
+  //console.log(calculadora);
+
+  var botaoLimpar = document.getElementById("botao-limpar");
+  botaoLimpar.addEventListener("click", limpar);
+
+  var botoesNumero = document.querySelectorAll("button.numero");
+
+
+  for (var i = 0; i < botoesNumero.length; i++) {
+    botoesNumero[i].addEventListener('click', valor)
+  }
+
+
 }
