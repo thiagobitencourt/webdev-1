8184ae558496d32d18299fb9536a4310205a064e
Fabio Lima <fabioholocheski@gmail.com>
2017-10-26 20:48:00 -0200

    Adiciona o comando onclick para os botoes

--- fabioholocheski/Calculadora-FabioLima/src/calculadora.js
+++ fabioholocheski/Calculadora-FabioLima/src/calculadora.js
@@ -8,6 +8,14 @@ var func = function(){
   console.log("função que esta na variavel");
 }
 
+function calcular() {
+  alert("Deve mostrar o resultado da operação")
+}
+function valor(element, evento, val) {
+  console.log(element);
+  console.log(evento);
+  alert("voce clicou no botao   "  + val);
+}
 window.onload = function(){
-  console.log("terminou de Carregar a Pagina");  
+  console.log("terminou de Carregar a Pagina");
 }
