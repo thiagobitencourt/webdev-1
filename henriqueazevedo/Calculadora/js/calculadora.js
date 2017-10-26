bbb2649ecd39952f569dd7e4fdbd3167125c7e14
Henrique Azevedo <hique2010@gmail.com>
2017-10-26 20:45:15 -0200

    novas funções calculadora

---
+++ henriqueazevedo/Calculadora/js/calculadora.js
@@ -0,0 +1,12 @@
+console.log("ola eu sou a calculadora java");
+
+function calcular(){
+	alert("voce clicou em calcular");
+	
+}
+
+function valor(element, evento, val){
+	console.log(element);
+	console.log(evento);
+	alert("voce clicou no botão "+ val)
+}
