5ac5ecbda9bd822a457893a06b44819d700fd80c
Henrique Azevedo <hique2010@gmail.com>
2017-10-26 21:44:23 -0200

    todo conteudo da aula do dia 26/10

--- henriqueazevedo/Calculadora/js/calculadora.js
+++ henriqueazevedo/Calculadora/js/calculadora.js
@@ -2,11 +2,37 @@ console.log("ola eu sou a calculadora java");
 
 function calcular(){
 	alert("voce clicou em calcular");
-	
+	/*var calculadora = document.querySelectorAll(".btns-line");
+	console.log(calculadora);*/
 }
 
-function valor(element, evento, val){
-	console.log(element);
-	console.log(evento);
-	alert("voce clicou no botão "+ val)
+function valor(evento){
+	console.log(this.value);
+	
+}
+function limpar(){
+	console.log("limparrr.....");
+}
+
+
+window.onload = function(){
+	
+	var botaoLimpar = document.getElementById("botao-limpar");
+	botaoLimpar.addEventListener("click", limpar);
+	
+	var botoesNumero = document.querySelectorAll("button.numero");
+	for(var i = 0; i < botoesNumero.length; i++){
+		botoesNumero[i].addEventListener('click', valor);
+	}
+	
+	
+	
+	
+	
+	
+	
+	var botoesOperacao = document.querySelectorAll("button.operacao");
+	for(var i = 0; i < botoesOperacao.length; i++){
+		botoesNumero[i].addEventListener('click', valor);
+	}
 }
