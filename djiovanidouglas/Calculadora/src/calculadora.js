4b54693aa65e6afdf5248aeceb3c11ac84a23c6f
unknown <djiovani.douglas@gmail.com>
2017-10-26 20:10:33 -0200

    Adicionado eventos no calculadora.js e ajustado campos no index

---
+++ djiovanidouglas/Calculadora/src/calculadora.js
@@ -0,0 +1,38 @@
+//console.log("Oi, eu sou o arquivo calculadora.js");
+
+// console.log(window);
+console.log(document.getElementById("calculadora-display"));
+
+function nomeDaFuncao(nome) {
+ console.log("Estou dentro da função...");
+ console.log("Recebi o parametro", nome);
+}
+// nomeDaFuncao("Meu nome");
+
+var func = function() {
+ console.log("Função que esta na variavel");
+}
+// func();
+
+// window.onload = func;
+window.onload = function() {
+ console.log("Terminou de carregar a página!");
+}
+
+// window.onload = function() {
+//
+// }
+
+function calcular() {
+ alert("Voce clicou em Calcular !!!");
+
+}
+
+function valor(element) {
+ console.log(element);
+
+}
+
+function operacao() {
+
+}
