84818f58ffbc022d2e8c0cc92bd51bac9b4ef785
Fabio Lima <fabioholocheski@gmail.com>
2017-10-26 19:57:36 -0200

    Primeiros passos JavaScript

---
+++ fabioholocheski/Calculadora-FabioLima/src/calculadora.js
@@ -0,0 +1,13 @@
+console.log(document.getElementById('calculadora-display'));
+
+function nomeDaFuncao() {
+  console.log("Estou dentro da função....");
+}
+
+var func = function(){
+  console.log("função que esta na variavel");
+}
+
+window.onload = function(){
+  console.log("terminou de Carregar a Pagina");  
+}
