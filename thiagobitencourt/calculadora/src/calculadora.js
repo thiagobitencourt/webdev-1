857af1873df574c110537bbd30640683832533ca
Thiago Bitencourt <thiago.mbitencourt@gmail.com>
2017-10-27 20:16:00 -0200

    Adiciona eventos a todos os botões de numeros

--- thiagobitencourt/calculadora/src/calculadora.js
+++ thiagobitencourt/calculadora/src/calculadora.js
@@ -1,24 +1,32 @@
-//console.log("Oi, eu sou o arquivo calculadora.js");
-
-// console.log(window);
-console.log(document.getElementById("calculadora-display"));
-
-function nomeDaFuncao(nome) {
-  console.log("Estou dentro da função...");
-  console.log("Recebi o parametro", nome);
+/*
+  Função que irá executar a operação requerida
+*/
+function calcular() {
+    alert("Deve mostrar o resultado da operação!");
 }
-// nomeDaFuncao("Meu nome");
 
-var func = function() {
-  console.log("Função que esta na variavel");
+/*
+  Função que é chamada sempre que um usuario
+  clica em um botão de numero da calculadora
+*/
+// function valor(element, evento) {
+function valor() {
+  console.log(this.value);
 }
-// func();
 
-// window.onload = func;
+function limpar() {
+  console.log("Limpar...");
+}
+
+// Função chamada quando a página terminar de carregar!
 window.onload = function() {
   console.log("Terminou de carregar a página!");
-}
 
-// window.onload = function() {
-//
-// }
+  var botaoLimpar = document.getElementById("botao-limpar");
+  botaoLimpar.addEventListener("click", limpar);
+
+  var botoesNumero = document.querySelectorAll("button.numero");
+  for(var i = 0; i < botoesNumero.length; i++) {
+    botoesNumero[i].addEventListener('click', valor);
+  }
+}
