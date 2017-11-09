5018877305416114b757a9272b7210a9cf0b893c
Adlan Luis Koslopp <adlanluis@hotmail.com>
2017-11-09 20:21:58 -0200

    Adiciona mais funcionalidades no botoes e no display

---
+++ adlanluiskoslopp/Calculadora/src/Calculadora.js
@@ -0,0 +1,72 @@
+var display;
+
+/*
+  Função que irá executar a operação requerida
+*/
+function calcular() {
+    alert("Deve mostrar o resultado da operação!");
+}
+
+/*
+  Função que é chamada sempre que um usuario
+  clica em um botão de numero da calculadora
+*/
+// function valor(element, evento) {
+function valor() {
+  var valorAtual = display.value;
+  display.value = valorAtual + this.value;
+}
+
+function definirOperacao() {
+  console.log(this.value);
+
+  var operacao;
+  switch(this.value) {
+    case "SOMAR":
+      operacao = "+";
+      break;
+    case "SUBTRAIR":
+      operacao = "-";
+      break;
+    case "MULTIPLICAR":
+      operacao = "X";
+      break;
+    case "DIVIDIR":
+      operacao = "/";
+      break;
+    default:
+      console.log("Operação desconhecida!");
+  }
+
+  display.value = display.value + operacao;
+}
+
+function limpar() {
+  display.value = "";
+}
+
+function ponto() {
+  console.log('Você clicou no "ponto"...');
+}
+
+// Função chamada quando a página terminar de carregar!
+window.onload = function() {
+  console.log("Terminou de carregar a página!");
+
+  var botaoLimpar = document.getElementById("botao-limpar");
+  botaoLimpar.addEventListener("click", limpar);
+
+  document.querySelector("button.ponto").addEventListener('click', ponto);
+
+  var botoesNumero = document.querySelectorAll("button.numero");
+  for(var i = 0; i < botoesNumero.length; i++) {
+    botoesNumero[i].addEventListener('click', valor);
+  }
+
+  var botoesOperacao = document.querySelectorAll("button.operacao");
+  for(var i = 0; i < botoesOperacao.length; i++) {
+    botoesOperacao[i].addEventListener('click', definirOperacao);
+  }
+
+  display = document.getElementById("calculadora-display");
+}
