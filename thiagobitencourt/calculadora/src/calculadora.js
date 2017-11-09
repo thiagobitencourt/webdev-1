14ee273892fb1cc562bf9b72358b67fead5626fa
Thiago Bitencourt <thiago.mbitencourt@gmail.com>
2017-11-09 19:32:46 -0200

    Adiciona eventos para botoes de operacao e ponto

--- thiagobitencourt/calculadora/src/calculadora.js
+++ thiagobitencourt/calculadora/src/calculadora.js
@@ -14,8 +14,16 @@ function valor() {
   console.log(this.value);
 }
 
+function definirOperacao() {
+  console.log(this.value);
+}
+
 function limpar() {
   console.log("Limpar...");
+}
+
+function ponto() {
+  console.log('Você clicou no "ponto"...');
 }
 
 // Função chamada quando a página terminar de carregar!
@@ -25,8 +33,15 @@ window.onload = function() {
   var botaoLimpar = document.getElementById("botao-limpar");
   botaoLimpar.addEventListener("click", limpar);
 
+  document.querySelector("button.ponto").addEventListener('click', ponto);
+
   var botoesNumero = document.querySelectorAll("button.numero");
   for(var i = 0; i < botoesNumero.length; i++) {
     botoesNumero[i].addEventListener('click', valor);
+  }
+
+  var botoesOperacao = document.querySelectorAll("button.operacao");
+  for(var i = 0; i < botoesOperacao.length; i++) {
+    botoesOperacao[i].addEventListener('click', definirOperacao);
   }
 }
